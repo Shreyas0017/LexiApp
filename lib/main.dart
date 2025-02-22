58fa44323fc1cf694ba71332fd09f1c51c2cf7fb
Shreyas0017 <shreyassaha00@gmail.com>
2025-02-22 18:05:50 +0530

    Landing UI updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,18 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const LexiLearnApp());
+  runApp(const LexiShiftApp());
 }
 
-class LexiLearnApp extends StatefulWidget {
-  const LexiLearnApp({super.key});
+class LexiShiftApp extends StatefulWidget {
+  const LexiShiftApp({super.key});
 
   @override
-  State<LexiLearnApp> createState() => _LexiLearnAppState();
+  State<LexiShiftApp> createState() => _LexiShiftAppState();
 }
 
-class _LexiLearnAppState extends State<LexiLearnApp> {
+class _LexiShiftAppState extends State<LexiShiftApp> {
   bool _useOpenDyslexicFont = false;
-  double _fontSize = 18.0;
-
-  static const double _minFontSize = 14.0;
-  static const double _maxFontSize = 32.0;
-  static const double _fontSizeStep = 2.0;
 
   void _toggleFont() {
     setState(() {
@@ -25,108 +20,229 @@ class _LexiLearnAppState extends State<LexiLearnApp> {
     });
   }
 
-  void _increaseFontSize() {
-    setState(() {
-      if (_fontSize < _maxFontSize) {
-        _fontSize += _fontSizeStep;
-      }
-    });
-  }
-
-  void _decreaseFontSize() {
-    setState(() {
-      if (_fontSize > _minFontSize) {
-        _fontSize -= _fontSizeStep;
-      }
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'LexiLearnApp',
+      title: 'LexiShift',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primaryColor: Colors.black,
+        scaffoldBackgroundColor: Colors.black,
+        textTheme: const TextTheme(
+          bodyLarge: TextStyle(color: Colors.white),
+          bodyMedium: TextStyle(color: Colors.white),
+        ),
         fontFamily: _useOpenDyslexicFont ? 'OpenDyslexic' : 'Roboto',
       ),
-      home: HomePage(
-        fontSize: _fontSize,
+      home: LandingPage(
         useOpenDyslexicFont: _useOpenDyslexicFont,
         onToggleFont: _toggleFont,
-        onIncreaseFontSize: _increaseFontSize,
-        onDecreaseFontSize: _decreaseFontSize,
       ),
     );
   }
 }
 
-class HomePage extends StatelessWidget {
-  final double fontSize;
+class LandingPage extends StatefulWidget {
   final bool useOpenDyslexicFont;
   final VoidCallback onToggleFont;
-  final VoidCallback onIncreaseFontSize;
-  final VoidCallback onDecreaseFontSize;
 
-  const HomePage({
+  const LandingPage({
     super.key,
-    required this.fontSize,
     required this.useOpenDyslexicFont,
     required this.onToggleFont,
-    required this.onIncreaseFontSize,
-    required this.onDecreaseFontSize,
+  });
+
+  @override
+  State<LandingPage> createState() => _LandingPageState();
+}
+
+class _LandingPageState extends State<LandingPage> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _fadeAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(seconds: 1),
+      vsync: this,
+    );
+    _fadeAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.easeIn),
+    );
+    _controller.forward();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [
+              Colors.black,
+              Colors.grey[900]!,
+              Colors.black,
+            ],
+          ),
+        ),
+        child: Stack(
+          children: [
+            FadeTransition(
+              opacity: _fadeAnimation,
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'LexiShift',
+                      style: TextStyle(
+                        fontSize: 48,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                        letterSpacing: 2,
+                        shadows: [
+                          Shadow(
+                            color: Colors.white.withOpacity(0.5),
+                            blurRadius: 10,
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    Text(
+                      'Welcome to LexiShift',
+                      style: TextStyle(
+                        fontSize: 24,
+                        color: Colors.grey[300],
+                        letterSpacing: 1.5,
+                      ),
+                    ),
+                    const SizedBox(height: 60),
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          PageRouteBuilder(
+                            pageBuilder: (context, animation, secondaryAnimation) =>
+                                MainPage(
+                                  useOpenDyslexicFont: widget.useOpenDyslexicFont,
+                                  onToggleFont: widget.onToggleFont,
+                                ),
+                            transitionsBuilder:
+                                (context, animation, secondaryAnimation, child) {
+                              return FadeTransition(
+                                opacity: animation,
+                                child: child,
+                              );
+                            },
+                            transitionDuration: const Duration(milliseconds: 500),
+                          ),
+                        );
+                      },
+                      style: ElevatedButton.styleFrom(
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 32,
+                          vertical: 16,
+                        ),
+                        backgroundColor: Colors.white,
+                        foregroundColor: Colors.black,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                      ),
+                      child: const Text(
+                        'Welcome to the New You',
+                        style: TextStyle(
+                          fontSize: 18,
+                          letterSpacing: 1,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Positioned(
+              top: 40,
+              right: 20,
+              child: AnimatedOpacity(
+                duration: const Duration(milliseconds: 500),
+                opacity: 1.0,
+                child: Switch(
+                  value: widget.useOpenDyslexicFont,
+                  onChanged: (_) => widget.onToggleFont(),
+                  activeColor: Colors.white,
+                  inactiveThumbColor: Colors.grey,
+                  activeTrackColor: Colors.grey[400],
+                  inactiveTrackColor: Colors.grey[800],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class MainPage extends StatelessWidget {
+  final bool useOpenDyslexicFont;
+  final VoidCallback onToggleFont;
+
+  const MainPage({
+    super.key,
+    required this.useOpenDyslexicFont,
+    required this.onToggleFont,
   });
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-          'LexiLearnApp',
-          style: TextStyle(fontSize: fontSize + 4),
+        backgroundColor: Colors.black,
+        title: const Text(
+          'LexiShift',
+          style: TextStyle(color: Colors.white),
         ),
+        actions: [
+          Container(
+            margin: const EdgeInsets.only(right: 16),
+            child: Switch(
+              value: useOpenDyslexicFont,
+              onChanged: (_) => onToggleFont(),
+              activeColor: Colors.white,
+              inactiveThumbColor: Colors.grey,
+              activeTrackColor: Colors.grey[400],
+              inactiveTrackColor: Colors.grey[800],
+            ),
+          ),
+        ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'This is a sample text with dynamic font!',
-              style: TextStyle(fontSize: fontSize),
-              textAlign: TextAlign.center,
-            ),
-            const SizedBox(height: 20),
-            Text(
-              'Another text to verify global font change.',
-              style: TextStyle(fontSize: fontSize),
-              textAlign: TextAlign.center,
-            ),
-            const SizedBox(height: 40),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                ElevatedButton(
-                  onPressed: onDecreaseFontSize,
-                  child: const Text('A-'),
-                ),
-                const SizedBox(width: 10),
-                ElevatedButton(
-                  onPressed: onIncreaseFontSize,
-                  child: const Text('A+'),
-                ),
-              ],
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: onToggleFont,
-              child: Text(
-                useOpenDyslexicFont
-                    ? 'Switch to Default Font'
-                    : 'Switch to OpenDyslexic Font',
-                textAlign: TextAlign.center,
-              ),
-            ),
-          ],
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [
+              Colors.black,
+              Colors.grey[900]!,
+              Colors.black,
+            ],
+          ),
+        ),
+        child: const Center(
+          child: Text(
+            'Main Page Content',
+            style: TextStyle(color: Colors.white),
+          ),
         ),
       ),
     );
